4f69b0c28069982b3d30d156d077cb22b8be7210
Ahmad Bintang <ahmadbintang812@gmail.com>
2026-05-29 18:41:41 +0700

    Add Vercel backend deployment support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,3 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
-
-
-
--- config/database.php
+++ config/database.php
@@ -3,6 +3,16 @@
 use Illuminate\Support\Str;
 use Pdo\Mysql;
 
+$mysqlSslCa = env('MYSQL_ATTR_SSL_CA');
+
+if (! $mysqlSslCa && env('MYSQL_CA_CERT')) {
+    $mysqlSslCa = sys_get_temp_dir().'/mysql-ca.pem';
+
+    if (! file_exists($mysqlSslCa)) {
+        file_put_contents($mysqlSslCa, env('MYSQL_CA_CERT'));
+    }
+}
+
 return [
 
     /*
@@ -60,7 +70,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => $mysqlSslCa,
             ]) : [],
         ],
 
