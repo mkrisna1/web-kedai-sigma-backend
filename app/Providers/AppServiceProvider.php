13ab26f829b719bfa86fb066e9fe56ed14b411e5
LuxStarr <ahmadbintang812@gmail.com>
2026-06-01 17:56:06 +0700

    Add serverless view binding fallback

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\View\Factory as ViewFactoryContract;
+use Illuminate\Contracts\View\View as ViewContract;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\View\ViewServiceProvider;
 
@@ -13,6 +15,89 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->register(ViewServiceProvider::class);
+
+        if (! $this->app->bound('view')) {
+            $this->app->singleton('view', fn () => new class implements ViewFactoryContract
+            {
+                public function exists($view): bool
+                {
+                    return false;
+                }
+
+                public function file($path, $data = [], $mergeData = []): ViewContract
+                {
+                    return $this->emptyView($path, $data, $mergeData);
+                }
+
+                public function make($view, $data = [], $mergeData = []): ViewContract
+                {
+                    return $this->emptyView($view, $data, $mergeData);
+                }
+
+                public function share($key, $value = null): mixed
+                {
+                    return $value;
+                }
+
+                public function composer($views, $callback): array
+                {
+                    return [];
+                }
+
+                public function creator($views, $callback): array
+                {
+                    return [];
+                }
+
+                public function addNamespace($namespace, $hints): static
+                {
+                    return $this;
+                }
+
+                public function replaceNamespace($namespace, $hints): static
+                {
+                    return $this;
+                }
+
+                private function emptyView(string $name, $data = [], array $mergeData = []): ViewContract
+                {
+                    return new class($name, array_merge((array) $mergeData, (array) $data)) implements ViewContract
+                    {
+                        public function __construct(
+                            private string $name,
+                            private array $data = [],
+                        ) {
+                        }
+
+                        public function render(): string
+                        {
+                            return '';
+                        }
+
+                        public function name(): string
+                        {
+                            return $this->name;
+                        }
+
+                        public function with($key, $value = null): static
+                        {
+                            if (is_array($key)) {
+                                $this->data = array_merge($this->data, $key);
+                            } else {
+                                $this->data[$key] = $value;
+                            }
+
+                            return $this;
+                        }
+
+                        public function getData(): array
+                        {
+                            return $this->data;
+                        }
+                    };
+                }
+            });
+        }
     }
 
     /**
